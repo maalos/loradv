68e7b9c44c603f94b5b9610f275d71ae8d2046d7
maalos <fasterd91@gmail.com>
2024-12-20 20:16:12 +0100

    this works

--- include/config.h
+++ include/config.h
@@ -83,12 +83,12 @@ extern void setupDisplay();
 #define LORA_RADIO_PIN_TXEN 0   // TXEN 7
 // LoRa params
 #define LORA_RADIO_FREQ 434.0   // initial frequency
-#define LORA_RADIO_BW   31.25   // bandwidth in kHz
+#define LORA_RADIO_BW   31.25    // bandwidth in kHz
 #define LORA_RADIO_SF   7       // spreading factor (SF12-10 is too slow for DV)
 #define LORA_RADIO_CR   5       // coding rate denominator
 #define LORA_RADIO_SYNC 0x34    // sync word
 #define LORA_RADIO_PWR  2       // power in dbm (real is +10db if module has amplifier)
-#define LORA_RADIO_PL   4U      // preamble length
+#define LORA_RADIO_PL   8       // preamble length
 #define LORA_RADIO_CRC  1       // length of the CRC in bytes
 #define LORA_RADIO_EXPL true    // comment out to use implicit mode (for spreading factor 6)
 
--- src/audio.cpp
+++ src/audio.cpp
@@ -1,57 +1,58 @@
 #include <config.h>
 
-void setupAudio() {
-  // setup speaker
-  i2s_config_t i2s_speaker_config = {
-    .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_TX),
-    .sample_rate = AUDIO_SAMPLE_RATE,
-    .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
-    .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
-    .communication_format = I2S_COMM_FORMAT_STAND_MSB,
-    .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
-    .dma_buf_count = 8, // 2
-    .dma_buf_len = 1024, // 32's good for 3200
-    .use_apll=0,
-    .tx_desc_auto_clear= true, 
-    .fixed_mclk=-1    
-  };
-  i2s_pin_config_t i2s_speaker_pin_config = {
-    .bck_io_num = AUDIO_SPEAKER_BCLK,
-    .ws_io_num = AUDIO_SPEAKER_LRC,
-    .data_out_num = AUDIO_SPEAKER_DIN,
-    .data_in_num = I2S_PIN_NO_CHANGE
-  };
-  if (i2s_driver_install(I2S_NUM_0, &i2s_speaker_config, 0, NULL) != ESP_OK) {
-    LOG_ERROR("Failed to install i2s speaker driver");
-  }
-  if (i2s_set_pin(I2S_NUM_0, &i2s_speaker_pin_config) != ESP_OK) {
-    LOG_ERROR("Failed to set i2s speaker pins");
-  }
+void setupAudio()
+{
+	// setup speaker
+	i2s_config_t i2s_speaker_config = {
+			.mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_TX),
+			.sample_rate = AUDIO_SAMPLE_RATE,
+			.bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
+			.channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
+			.communication_format = I2S_COMM_FORMAT_STAND_MSB,
+			.intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
+			.dma_buf_count = 8,  // 2
+			.dma_buf_len = 1024, // 32's good for 3200
+			.use_apll = 0,
+			.tx_desc_auto_clear = true,
+			.fixed_mclk = -1};
+	i2s_pin_config_t i2s_speaker_pin_config = {
+			.bck_io_num = AUDIO_SPEAKER_BCLK,
+			.ws_io_num = AUDIO_SPEAKER_LRC,
+			.data_out_num = AUDIO_SPEAKER_DIN,
+			.data_in_num = I2S_PIN_NO_CHANGE};
+	if (i2s_driver_install(I2S_NUM_0, &i2s_speaker_config, 0, NULL) != ESP_OK)
+	{
+		LOG_ERROR("Failed to install i2s speaker driver");
+	}
+	if (i2s_set_pin(I2S_NUM_0, &i2s_speaker_pin_config) != ESP_OK)
+	{
+		LOG_ERROR("Failed to set i2s speaker pins");
+	}
 
-  // setup microphone
-  i2s_config_t i2s_mic_config = {
-    .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX),
-    .sample_rate = AUDIO_SAMPLE_RATE,
-    .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
-    .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
-    .communication_format = I2S_COMM_FORMAT_STAND_I2S,
-    .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
-    .dma_buf_count = 8, // 2
-    .dma_buf_len = 1024, //128
-    .use_apll=0,
-    .tx_desc_auto_clear= true,
-    .fixed_mclk=-1
-  };
-  i2s_pin_config_t i2s_mic_pin_config = {
-    .bck_io_num = AUDIO_MIC_SCK,
-    .ws_io_num = AUDIO_MIC_WS,
-    .data_out_num = I2S_PIN_NO_CHANGE,
-    .data_in_num = AUDIO_MIC_SD
-  };
-  if (i2s_driver_install(I2S_NUM_1, &i2s_mic_config, 0, NULL) != ESP_OK) {
-    LOG_ERROR("Failed to install i2s mic driver");
-  }
-  if (i2s_set_pin(I2S_NUM_1, &i2s_mic_pin_config) != ESP_OK) {
-    LOG_ERROR("Failed to set i2s mic pins");
-  }
+	// setup microphone
+	i2s_config_t i2s_mic_config = {
+			.mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX),
+			.sample_rate = AUDIO_SAMPLE_RATE,
+			.bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
+			.channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
+			.communication_format = I2S_COMM_FORMAT_STAND_I2S,
+			.intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
+			.dma_buf_count = 8,  // 2
+			.dma_buf_len = 1024, // 128
+			.use_apll = 0,
+			.tx_desc_auto_clear = true,
+			.fixed_mclk = -1};
+	i2s_pin_config_t i2s_mic_pin_config = {
+			.bck_io_num = AUDIO_MIC_SCK,
+			.ws_io_num = AUDIO_MIC_WS,
+			.data_out_num = I2S_PIN_NO_CHANGE,
+			.data_in_num = AUDIO_MIC_SD};
+	if (i2s_driver_install(I2S_NUM_1, &i2s_mic_config, 0, NULL) != ESP_OK)
+	{
+		LOG_ERROR("Failed to install i2s mic driver");
+	}
+	if (i2s_set_pin(I2S_NUM_1, &i2s_mic_pin_config) != ESP_OK)
+	{
+		LOG_ERROR("Failed to set i2s mic pins");
+	}
 }
